a6ca245eb3273dace1bd8e5f0158a50ec0d430c7
Vincent Turnier <vincent@lavinou.com>
2025-01-01 14:21:30 -0500

    removed files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/vincentturnier/lavinou/startpoint/startpoint-webdocument/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/vincentturnier/lavinou/startpoint/startpoint-webdocument/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/vincentturnier/lavinou/startpoint/startpoint-webdocument/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/vincentturnier/lavinou/startpoint/startpoint-webdocument/src/css/custom.css"),
-];
