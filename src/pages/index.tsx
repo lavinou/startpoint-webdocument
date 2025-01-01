7753debcd54006ee82a85cf489da1c6a9c1bc8ce
Vincent Turnier <vincent@lavinou.com>
2025-01-01 14:26:41 -0500

    removed nice

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Nice StartPoint Tutorial - 5min ⏱️
+            StartPoint Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
